ee0b3a135d867183745bb2ae37f0ea4a72467cf8
AnnaTykhomyrova <tykhomyrovaanna@gmail.com>
2019-06-04 22:12:49 +0000

    URL Shortening without POST

--- express_server.js
+++ express_server.js
@@ -1,6 +1,13 @@
 var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+function generateRandomString() {
+  let r = Math.random().toString(36).substring(7);
+   return r;
+}
 
 app.set("view engine", "ejs");
 
@@ -19,6 +26,15 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls", (req, res) => {
